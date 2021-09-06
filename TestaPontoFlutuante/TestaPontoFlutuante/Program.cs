d59a4cceb8eaafd599fb25f2017182aa102438d4
Matheus Passatuto <50487734+matheusdomepass@users.noreply.github.com>
2021-09-06 08:41:51 -0300

    Add files via upload

---
+++ TestaPontoFlutuante/TestaPontoFlutuante/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestaPontoFlutuante
+{
+    class Program
+    {
+        static void Main(String [] args)
+        {
+            double salario = 1250.70;
+
+            Console.WriteLine(salario);
+
+
+            Console.WriteLine("meu salario é " + salario);
+            Console.ReadLine();
+
+        }
+    }
+}
